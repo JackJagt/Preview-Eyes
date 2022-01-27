f761968020c6b5c3b11122cabd68380b390bdf6d
elprofesorrr <81437737+elprofesorrr@users.noreply.github.com>
2022-01-27 19:40:53 +0100

    commentaar toegevoegd ML
    commentaar toegevoegd ML ( index.html en ML.dart) en dat lege mapje even verplaatst

--- buddybot/BuddyBotCam/lib/ML/ML.dart
+++ buddybot/BuddyBotCam/lib/ML/ML.dart
@@ -2,34 +2,28 @@ import 'dart:convert';
 
 import 'package:buddy_bot_cam/VisionDetectorViews/face_detector_view.dart';
 import 'package:flutter/material.dart';
-//import 'package:permission_handler/permission_handler.dart';
 import 'package:teachable/teachable.dart';
  
 
 class ML extends StatefulWidget {
-  // ML({Key? key, required this.title}) : super(key: key);
-
   late final String title;
 
   @override
   _MLState createState() => _MLState();
 
-  //await Permission.camera.request();
-  //await Permission.microphone.request();
 }
 
 class _MLState extends State<ML> {
-  int pose1 = 0;
-  int pose2 = 0;
-  int pose3 = 0;
+  int pose1 = 0; // percentage answer 1
+  int pose2 = 0; // percentage answer 2
+  int pose3 = 0; // percentage of the most true answer
 
-  String label = "test";
+  String label = "Loading"; // waiting until ML loaded
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Jouw antwoord")),
-
+        appBar: AppBar(title: Text("Your answer")),
         body: Stack(
           children: [
             Container(
@@ -37,23 +31,28 @@ class _MLState extends State<ML> {
               Expanded(
                 child: Container(
                   child: Teachable(
-                    path: "pose/index.html",
+                    path: "pose/index.html", // path  to model ml
                     results: (res) {
                       var resp = jsonDecode(res);
 
                       setState(() {
-                        pose1 = (resp['Ja'] * 100.0).round();
-                        pose2 = (resp['Niks'] * 100.0).round();
+                        pose1 = (resp['Ja'] * 100.0).round(); //percentage answer 1
+                        pose2 = (resp['Niks'] * 100.0).round(); //percentage answer 2
 
+                        // if the value of pose 1 (yes) is greater than pose 2 (nothing) and the answer is therefore yes
                         if (pose1 > pose2) {
                           pose3 = pose1;
                           label = "Yes";
+
+                          //go back to FaceDetectorView if the answer is yes
                           //Navigator.pop(context);
                           Navigator.pushReplacement(
                             context,
                             MaterialPageRoute(builder: (context) => FaceDetectorView()),
                           );
                         }
+
+                        // if the answer is nothing or no
                         else{
                           pose3 = pose2;
                           label = "Nothing";
@@ -64,8 +63,9 @@ class _MLState extends State<ML> {
                 ),
               ),
             ])),
-            Align(
 
+            // print the answer
+            Align(
               alignment: Alignment.bottomCenter,
               child: Container(
                   width: double.infinity,
